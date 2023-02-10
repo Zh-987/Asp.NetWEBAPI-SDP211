77f98facb31a6b0e62f4aa13c12bf0c70adcc085
77001803540 <zhasulanasainov@gmail.com>
2023-02-10 21:03:33 +0500

    10/2/2023 Added by Zhassulan

---
+++ SDP211-WEB_API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SDP211_WEB_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "PostWeatherForecast")]
+        public void Post()
+        {
+           
+        }
+
+        [HttpPut(Name = "PutWeatherForecast")]
+        public void Put(int id)
+        {
+
+        }
+
+        [HttpDelete(Name = "DeleteWeatherForecast")]
+        public void Delete(int id)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ SDP211-WEB_API/Models/WeatherContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SDP211_WEB_API.Models
+{
+    public class WeatherContext : DbContext
+    {
+        public DbSet<WeatherForecast> weatherForecast { get; set; }   
+    }
+}
